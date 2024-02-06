749c524b50600ced6830aa6415a90cd541c79531
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-02-06 10:57:27 -0300

    arrumei

--- php/index.php
+++ php/index.php
@@ -5,6 +5,11 @@ require_once 'TriviaAPIParaBanco.php';
 require_once 'PedeParaBanco.php';
 require_once 'GeradorToken.php';
 
+//prontas
+//triviaAPIParaBanco adaptar para retornar true ou false para a pede banco
+//GeradorToken
+//PedeParaBanco
+
 // A classe TriviaAPIParaBanco será ajustada para usar a classe Conexao internamente
 $tokenGenerator = new GeradorToken();
 $sessionToken = $tokenGenerator->geraNovoToken();
@@ -16,11 +21,12 @@ $triviaAPIParaBanco->fetchAndSaveQuestion();
 // Utiliza false para pergunta aleatória ou true para a última pergunta
 $buscaPergunta = new PedeParaBanco(false); // Passando false para exemplo de pergunta aleatória
 
-echo "Pergunta: " . $buscaPergunta->pegaPergunta() . "\n";
+/*echo "Pergunta: " . $buscaPergunta->pegaPergunta() . "\n";
 echo "Dificuldade: " . $buscaPergunta->pegaDificuldade() . "\n";
 echo "Resposta Correta: " . $buscaPergunta->pegaRespostaCorreta() . "\n";
 echo "Respostas Erradas: " . implode(", ", $buscaPergunta->pegaRespostasErradas()) . "\n"; // Use implode para juntar as respostas erradas, se elas forem um array
-echo "Tipo: " . $buscaPergunta->pegaTipo() . "\n";
+echo "Tipo: " . $buscaPergunta->pegaTipo() . "\n";*/
 
 // botao retornar numero de 1 a 4 para testar se acertou
 
+?>
