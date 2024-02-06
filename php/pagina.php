b5d36fabc82ae915eb374b0902133952066c1d94
CaioBalc <balczarekcaio@gmail.com>
2024-02-06 08:54:53 -0300

    Página

---
+++ php/pagina.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <title>Desafio Trivia</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <Header>
+            <h1 class="centralizar">TRIVIA</h1>
+        </Header>
+
+        <section>
+            <h2>Tipo: </h2>
+            <h2>Dificuldade: </h2>
+        </section>
+</body>
+</html>
